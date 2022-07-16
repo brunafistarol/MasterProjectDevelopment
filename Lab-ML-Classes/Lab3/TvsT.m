8ab5d98c68434838b5124beee689adcbcbbab96e
Bruna Fistarol <30641224+brunafistarol@users.noreply.github.com>
2022-07-16 15:03:38 -0300

    Delete Lab-ML-Classes directory

--- Lab-ML-Classes/Lab3/TvsT.m
+++
@@ -1,8 +0,0 @@
-function[points_train, points_test] = TvsT(input_data)
-
-%separate input_data into a train set and a test set
-[m,n] = size(input_data);
-id = randperm(m, ceil(0.8*m)); %you could change the 0.8 in order to change the proportion between train and test sets
-points_train = input_data(id,:);
-points_test = input_data(setdiff(1:m,id),:);
-end
--- Lab-ML-Classes/Lab3/accuracy.m
+++
@@ -1,8 +0,0 @@
-function [acc,acc_perclass,classes] = accuracy(y,pred)
-%the input y and pred should be columns, y are the actual (real) classes
-%for each point meanwhile pred is a vector with the predcitions made for
-%some model for the same points
-acc = (1/length(y))*sum(y==pred); %proportion of the quantinty of well predicted points in general
-classes = unique(y)';
-acc_perclass = (1/length(y))*sum((y==classes)==(pred==classes)); %proportion of the quantinty of well predicted points in each class
-end
